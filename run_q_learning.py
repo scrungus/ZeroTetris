660ff23165b866eb7e2eacdc992138c3d3a87d67
OliverOverend <68807336+OliverOverend@users.noreply.github.com>
2021-10-02 15:43:38 +0100

    feat(qlearning): Implement run_q_learning
    run_q_learning trains and evaluates a Q-learning agent.

---
+++ run_q_learning.py
@@ -0,0 +1,34 @@
+from gym_simplifiedtetris.helpers import train_q_learning
+from gym_simplifiedtetris.envs import SimplifiedTetrisBinaryEnv as Tetris
+from gym_simplifiedtetris.agents import QLearningAgent
+from gym_simplifiedtetris.helpers.eval_agent import eval_agent
+
+
+def main():
+    grid_dims = (7, 4)
+
+    env = Tetris(
+        grid_dims=grid_dims,
+        piece_size=3,
+    )
+
+    q_table_dims = [2 for _ in range(grid_dims[0] * grid_dims[1])]
+    q_table_dims += [env._num_pieces_] + [env._num_actions_]
+
+    agent = QLearningAgent(q_table_dims)
+
+    agent = train_q_learning(
+        env=env,
+        agent=agent,
+        num_eval_timesteps=10000,
+    )
+
+    eval_agent(
+        agent=agent,
+        env=env,
+        num_eval_episodes=30,
+        render=True,
+    )
+
+if __name__ == '__main__':
+    main()
