7afdeee44135974f8ee43e1b49f6322fb67c8f2f
OliverOverend <68807336+OliverOverend@users.noreply.github.com>
2021-10-04 14:41:37 +0100

    refactor(import): Reposition imports

--- example.py
+++ example.py
@@ -1,9 +1,12 @@
+import gym
+
+import gym_simplifiedtetris
+
+
 def run_example_2():
     """
     Usage example 2.
     """
-    import gym_simplifiedtetris
-
     env = gym_simplifiedtetris.envs.SimplifiedTetrisBinaryEnv(
         grid_dims=(20, 10),
         piece_size=4,
@@ -14,19 +17,14 @@ def run_example_1():
     """
     Usage example 1.
     """
-    import gym
-
-    import gym_simplifiedtetris
-
     env = gym.make("simplifiedtetris-binary-v0")
-    agent = gym_simplifiedtetris.agents.UniformAgent(env._num_actions_)
     obs = env.reset()
 
     # Run 10 games of Tetris, selecting actions uniformly at random.
     num_episodes = 0
     while num_episodes < 10:
         env.render()
-        action = agent.predict()
+        action = env.action_space.sample()
         obs, rwd, done, info = env.step(action)
 
         if done:
