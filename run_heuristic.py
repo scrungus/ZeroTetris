ec2fc0510ee49cbb815cff37d263ce98f3b2166a
OliverOverend <68807336+OliverOverend@users.noreply.github.com>
2021-10-04 19:33:14 +0100

    refactor(score): Output mean score

--- run_heuristic.py
+++ run_heuristic.py
@@ -1,11 +1,15 @@
+import numpy as np
+
 from gym_simplifiedtetris.agents import DellacherieAgent
 from gym_simplifiedtetris.envs import SimplifiedTetrisBinaryEnv as Tetris
 
 
 def main():
+    ep_returns = np.zeros(10)
+
     agent = DellacherieAgent()
     env = Tetris(
-        grid_dims=(20, 10),
+        grid_dims=(8, 10),
         piece_size=4,
     )
     obs = env.reset()
@@ -18,6 +22,7 @@ def main():
 
         action = agent.predict(heuristic_scores)
         obs, rwd, done, info = env.step(action)
+        ep_returns[num_episodes] += info["num_rows_cleared"]
 
         if done:
             print(f"Episode {num_episodes + 1} has terminated.")
@@ -25,6 +30,11 @@ def main():
             obs = env.reset()
 
     env.close()
+
+    print(
+        f"\nScore obtained from averaging over {num_episodes} "
+        f"games: {np.mean(ep_returns):.1f} +/- {np.std(ep_returns):.1f}"
+    )
 
 
 if __name__ == "__main__":
--- run_uniform.py
+++ run_uniform.py
@@ -1,9 +1,12 @@
+import numpy as np
 import gym
 
 import gym_simplifiedtetris
 
 
 def main():
+    ep_returns = np.zeros(10)
+
     env = gym.make("simplifiedtetris-binary-v0")
     agent = gym_simplifiedtetris.agents.UniformAgent(env._num_actions_)
 
@@ -15,6 +18,7 @@ def main():
         env.render()
         action = agent.predict()
         obs, rwd, done, info = env.step(action)
+        ep_returns[num_episodes] += info["num_rows_cleared"]
 
         if done:
             print(f"Episode {num_episodes + 1} has terminated.")
@@ -22,6 +26,11 @@ def main():
             obs = env.reset()
 
     env.close()
+
+    print(
+        f"\nScore obtained from averaging over {num_episodes} "
+        f"games: {np.mean(ep_returns):.1f} +/- {np.std(ep_returns):.1f}"
+    )
 
 
 if __name__ == "__main__":
