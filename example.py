9bfee84d6ef4ae4daf850e241d39b7ba77a00097
Oliver Overend <ollyoverend10@gmail.com>
2021-10-13 19:02:49 +0100

    refactor: Add variable

--- example.py
+++ example.py
@@ -21,15 +21,16 @@ def run_example_1():
     obs = env.reset()
 
     # Run 10 games of Tetris, selecting actions uniformly at random.
-    num_episodes = 0
-    while num_episodes < 10:
+    num_episodes = 10
+    episode_num = 0
+    while episode_num < 10:
         env.render()
         action = env.action_space.sample()
         obs, rwd, done, info = env.step(action)
 
         if done:
-            print(f"Episode {num_episodes + 1} has terminated.")
-            num_episodes += 1
+            print(f"Episode {episode_num + 1} has terminated.")
+            episode_num += 1
             obs = env.reset()
 
     env.close()
