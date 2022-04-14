0e5c3898ea0ca5f8718c4c3377d6d3b6891f2119
scrunguss <drevil629@gmail.com>
2022-04-14 18:47:42 +0100

    dqn simpl sneg

--- TetrisWrapper.py
+++ TetrisWrapper.py
@@ -1,7 +1,7 @@
 import gym
 from gym_simplifiedtetris.envs import SimplifiedTetrisBinaryEnv as Tetris
 import csv
-import os
+
 
 def pickFileName():
 
@@ -14,7 +14,6 @@ class TetrisWrapper(Tetris):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.n = 1
-        self.score_types = [0]*4
 
         f = open('/log/score_count{}.txt'.format(pickFileName()), 'w+')
         self.writer = csv.writer(f)
@@ -29,25 +28,14 @@ class TetrisWrapper(Tetris):
         shaped_reward = 0
 
         if done:
-            shaped_reward = self._engine._score
-
-        elif reward == 1:
-            shaped_reward = 40
-            self.score_types[0] +=1
-        elif reward == 2:
-            shaped_reward = 100
-            self.score_types[1] +=1
-        elif reward == 3:
-            shaped_reward = 300
-            self.score_types[2] +=1
-        elif reward == 4:
-            shaped_reward = 1200
-            self.score_types[3] +=1
+            shaped_reward = -10
+        elif reward >= 1:
+            shaped_reward = reward)
 
         return obs, shaped_reward, done, info
 
+
     def epoch_lines(self):
          self.writer.writerow(self.score_types)
-         self.score_types = [0]*4
-
+         self.score_types.clear()
 
